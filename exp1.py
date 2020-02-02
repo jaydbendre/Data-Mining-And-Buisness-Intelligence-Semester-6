388715234ee677d2a9a21ae5b56404caaac329cf
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-02-02 17:18:38 +0530

    Tets

--- exp1.py
+++ exp1.py
@@ -56,3 +56,57 @@
 
 # # clean_csv()
 # # # operations()
+
+
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+import datetime as dt
+
+
+def update_delay(value):
+    current_time = pd.to_datetime(value["Time"], format="%I:%M %p")
+    status = value["Status"].split(" ")
+    if len(status) > 1:
+        if status[1] != "to":
+            arrival_time = pd.to_datetime(
+                status[1] + " " + status[2], format="%I:%M %p"
+            )
+            return pd.Timedelta(abs(current_time - arrival_time)).seconds / 60
+    else:
+        return 0
+
+
+def update_status(value):
+    """
+    0 : Landed
+    1 : Canceled
+    2 : Diverted
+    """
+
+    status = value.split(" ")
+    if status[0] == "Landed":
+        return 0
+    elif status[0] == "Canceled":
+        return 1
+    else:
+        return 2
+
+
+def insert_date(value):
+    date = value.split(" ")
+    if len(date) > 1 and date[1] != "to":
+        return pd.to_datetime(date[1] + " " + date[2], format="%I:%M %p").time()
+    else:
+        return np.nan
+
+
+data = pd.read_csv("Datasets/AirplaneData26-01-2020.csv")
+data = pd.DataFrame(data[["Time", "Source", "Flight Name", "Status"]])
+data["delay"] = data[["Time", "Status"]].apply(update_delay, 1)
+data["Arrival"] = data["Status"].apply(insert_date, 1)
+data["Status"] = data["Status"].apply(update_status, 1)
+change_time = lambda x: pd.to_datetime(x, format="%I:%M %p").time()
+data["Time"] = data["Time"].map(change_time)
+print(data.head(500))
