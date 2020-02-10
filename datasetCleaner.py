6387412e37d45e0d8d6f93e5b00f4e55c7053900
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-02-10 08:32:05 +0530

    Datetime

--- datasetCleaner.py
+++ datasetCleaner.py
@@ -5,21 +5,31 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 import datetime as dt
 from scipy.stats import chisquare, chi2_contingency
 
+"""
+Functions to update across the dataset
+"""
 
+"""
+Update Departure Source in the dataset
+"""
 def update_source(value):
     if value["type"] == "D":
         return "Mumbai"
     else:
         return value["Source"]
 
-
+"""
+Update Arrival Destination in the dataset
+"""
 def update_destination(value):
     if value["type"] == "A":
         return "Mumbai"
     else:
         return value["Destination"]
 
-
+"""
+Convert String of date and time to timestamp
+"""
 def update_timestamp_init(value):
     timestamp = str(value["date"]) + " " + str(value["Time"])
     timestamp = pd.to_datetime(timestamp, format="%Y-%m-%d %H:%M:%S")
@@ -30,21 +40,39 @@ def update_actual_time(value):
     status = value["Status"].split(" ")
     if len(status) == 3 and status[1] != "to":
         time = value["date"] + " " + status[1] + " " + status[2]
+        
         timestamp = pd.to_datetime(time, format="%Y-%m-%d %I:%M %p")
-        timedelta = timestamp - pd.to_datetime(
-            value["date"], format="%Y-%m-%d %H:%M:%S"
-        )
-        if timedelta > dt.timedelta(hours=12):
-            value["date"] = pd.to_datetime(
-                value["date"], format="%Y-%m-%d"
-            ) - dt.timedelta(days=1)
+        
+        date_timestamp =  pd.to_datetime(
+            value["Time"], format="%Y-%m-%d %H:%M:%S")
+        # print(timestamp,date_timestamp)
+        timedelta = pd.Timedelta(timestamp -date_timestamp).seconds/3600
+        # timedelta = dt.timedelta(timedelta)
+        # print(timedelta)
+        # print(dt.timedelta(hours=12))
+        hour_delay = pd.Timedelta(pd.to_datetime("12:00:00",format="%H:%M:%S") - pd.to_datetime("00:00:00",format="%H:%M:%S")).seconds/3600
+        
+        if timedelta > hour_delay:
+            # print("Hi")
+            if timestamp > date_timestamp:
+                print("Hi")
+                value["date"] = pd.to_datetime(
+                    value["date"], format="%Y-%m-%d"
+                ) + dt.timedelta(days=1)
+            else:
+                value["date"] = pd.to_datetime(
+                    value["date"], format="%Y-%m-%d"
+                )+ dt.timedelta(days=1)
             value["date"] = value["date"].date()
             time = str(value["date"]) + " " + status[1] + " " + status[2]
             timestamp = pd.to_datetime(time, format="%Y-%m-%d %I:%M %p")
             timedelta = timestamp - pd.to_datetime(
                 value["date"], format="%Y-%m-%d %H:%M:%S"
             )
-        return timestamp
+            print(timestamp)
+            return timestamp
+        else:
+            return timestamp
     else:
         return np.nan
 
