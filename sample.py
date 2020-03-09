6ee3966d446df710d5d13288158e17514f1f6451
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-03-09 09:57:51 +0530

    Sample

--- sample.py
+++ sample.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import random
 
-df = pd.read_csv(
+
+def delay_status(value):
+    if value > 15:
+        return "Delayed"
+    else:
+        return "On time"
+
+
+df = pd.DataFrame(pd.read_csv(
     "Datasets/FinalMergedDataset/cleaned_dataset.csv",
     skiprows=lambda i: i > 0 and random.random() > 0.01)
+)
+
+df["Delay_Status"] = df["Delay"].apply(delay_status, 1)
 
 with open("Datasets/FinalMergedDataset/sample_dataset.csv", "w") as csv_file:
     df.to_csv(path_or_buf=csv_file, index=False)
